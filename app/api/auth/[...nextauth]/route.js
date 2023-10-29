368ea03c4845382db32def4e0f1d57e948a65dbb
Cintia Michel <21688638@students.latrobe.edu.au>
2023-10-29 21:24:45 +1100

    Redo variables for google auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,6 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       secret: process.env.NEXTAUTH_SECRET,
-      issuer: process.env.NEXTAUTH_CLIENT_ISSUER
     })
   ]
 
